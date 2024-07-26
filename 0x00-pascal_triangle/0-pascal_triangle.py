359df5d398b6eb8ecbaf7dd63d643bed5ca696aa
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2024-07-26 14:02:01 +0300

    0x00-pascal_triangle/0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,6 +3,7 @@
 Pascal triangle
 """
 
+
 def pascal_triangle(n):
     """pascal_triangle function
 
@@ -23,5 +24,5 @@ def pascal_triangle(n):
             row.append(triangle[i-1][j-1] + triangle[i-1][j])
         row.append(1)
         triangle.append(row)
-    
+
     return triangle
