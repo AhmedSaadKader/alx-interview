a49fa2cf8b99066b6ecb123944e4b658e528ffe3
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2024-09-22 14:11:01 +0300

    making change

---
+++ 0x08-making_chanage.py/0-making_change.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""Make Change Algorithm"""
+
+
+def makeChange(coins, total):
+    """
+    make change function
+    """
+    if not coins or coins is None:
+        return -1
+    if total <= 0:
+        return 0
+    change = 0
+    coins = sorted(coins)[::-1]
+    for coin in coins:
+        while coin <= total:
+            total -= coin
+            change += 1
+        if (total == 0):
+            return change
+    return -1
