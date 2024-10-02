627a7978edbfed7aa247564021f6bd2a1c8d85db
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2024-10-02 20:01:42 +0300

    0x09-island_perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -10,10 +10,6 @@ This script calculates the perimeter of a 2D island
 def calculate_island_perimeter(land_grid):
     """
     Determines the perimeter of the island in the given grid.
-    Args:
-        land_grid: A 2D list of integers (0 or 1)
-    Returns:
-        The perimeter of the island.
     """
 
     perimeter = 0
