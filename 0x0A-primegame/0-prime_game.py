d98a40f38df2d97d1e8ec5214df466cb691b3f72
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2024-10-02 19:44:55 +0300

    prime game

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Prime Game"""
+
+def isWinner(rounds, nums):
+  """isWinner function"""
+  if rounds <= 0 or nums is None:
+    return None
+  if rounds != len(nums):
+    return None
+
+  ben = 0
+  maria = 0
+
+  one_list = [1] * (sorted(nums)[-1] + 1)
+
+  one_list[0], one_list[1] = 0, 0
+  for i in range(2, len(one_list)):
+    for j in range(2, len(one_list)):
+        try:
+            one_list[i * j] = 0
+        except (ValueError, IndexError):
+            break
+  
+  for i in nums:
+    if sum(one_list[0:i + 1]) % 2 == 0:
+      ben += 1
+    else:
+      maria += 1
+  
+  if ben > maria:
+    return "Ben"
+  
+  if maria > ben:
+    return "Maria"
+
+  return None
